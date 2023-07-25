8c53cf017b244e9a8830df24613bd53aaa11ede4
daniil <daniilzhilyaev@gmail.com>
2023-07-25 16:12:59 +0300

    add square programm

---
+++ 1_Square/Program.cs
@@ -0,0 +1,12 @@
+﻿// напишите программу, которая на вход принимает число и выдает его квадрат
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+// Математический способ:
+int sqr = number * number;
+Console.WriteLine($"Квадрат числа: {sqr}");
+
+// Библиотечый способ:
+int sqr1 = Convert.ToInt32(Math.Pow(number, 2));
+Console.WriteLine($"Квадрат числа: {sqr1}");
