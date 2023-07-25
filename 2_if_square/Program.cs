76c0752c3949a4669fa7c1a531459967ae90f1c0
daniil <daniilzhilyaev@gmail.com>
2023-07-25 16:39:45 +0300

    add if square programm

---
+++ 2_if_square/Program.cs
@@ -0,0 +1,19 @@
+﻿/* 1. Напишите программу, которая на вход принимает два числа и проверяет, является ои первое число квадратом второго
+
+a = 25; b = 5 -> yes
+a = 2; b = 10 -> no */
+
+Console.Write("Введите первое число: ");
+int number_1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите второе число: ");
+int number_2 = int.Parse(Console.ReadLine()!);
+
+if(number_1 == Convert.ToInt32(Math.Pow(number_2, 2)))
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+        Console.WriteLine("No");
+}
